136f80de645d8b4d77a8081fdbb7f77738650f14
alejandro <alejandro.amendola@gmail.com>
2021-02-18 13:55:16 -0300

    Automatic commit message

--- impl/rabbit.go
+++ impl/rabbit.go
@@ -35,11 +35,11 @@ func MakeClient(host, user, password, queue string, blacklist ...string) Client 
 // StartConsuming ...
 func (c Client) StartConsuming(consumer queuer.Consumer) {
 	conn, err := amqp.Dial(c.uri)
-	logutils.Fail(err, "Failed to connect to RabbitMQ")
+	logutils.Fatal(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	logutils.Fail(err, "Failed to open a channel")
+	logutils.Fatal(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
@@ -50,14 +50,14 @@ func (c Client) StartConsuming(consumer queuer.Consumer) {
 		false,   // no-wait
 		nil,     // arguments
 	)
-	logutils.Fail(err, "Failed to declare a queue")
+	logutils.Fatal(err, "Failed to declare a queue")
 
 	err = ch.Qos(
 		1,     // prefetch count
 		0,     // prefetch size
 		false, // global
 	)
-	logutils.Fail(err, "Failed to set QoS")
+	logutils.Fatal(err, "Failed to set QoS")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -68,7 +68,7 @@ func (c Client) StartConsuming(consumer queuer.Consumer) {
 		false,  // no-wait
 		nil,    // args
 	)
-	logutils.Fail(err, "Failed to register a consumer")
+	logutils.Fatal(err, "Failed to register a consumer")
 
 	forever := make(chan bool)
 
@@ -76,13 +76,13 @@ func (c Client) StartConsuming(consumer queuer.Consumer) {
 		for d := range msgs {
 			var dat map[string]interface{}
 			err := json.Unmarshal(d.Body, &dat)
-			logutils.Fail(err)
+			logutils.Fatal(err)
 
 			message := queuer.Message{}
 			json.Unmarshal(d.Body, &message)
 
 			err = consumer.Process(message)
-			logutils.Fail(err)
+			logutils.Fatal(err)
 
 			err = ch.Publish(
 				"",        // exchange
@@ -94,7 +94,7 @@ func (c Client) StartConsuming(consumer queuer.Consumer) {
 					CorrelationId: d.CorrelationId,
 					Body:          []byte(fmt.Sprintf("The file %s is up for to be downloaded", message.Path)),
 				})
-			logutils.Fail(err, "Failed to publish a message")
+			logutils.Fatal(err, "Failed to publish a message")
 
 			d.Ack(false)
 		}
